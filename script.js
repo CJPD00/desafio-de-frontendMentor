6d2e51c678dcf262a916413f9c2eba931195e3f6
CJP <carlosjavierpacheco60@gmail.com>
2023-03-07 14:52:50 -0500

    challenge complete

---
+++ script.js
@@ -0,0 +1,19 @@
+let columnaActiva=1
+
+const columnas=document.querySelectorAll(".columna")
+
+columnas.forEach((columna,indice)=>{
+
+    columna.addEventListener("click",function(){
+        cambiarcolumna(indice)
+    })
+
+})
+
+function cambiarcolumna(indice){
+
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva=indice
+
+}
